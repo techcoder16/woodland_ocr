6af9ef40b9fa1a64c8478e767d3f416c7063ad36
unknown <abbasi.work22@gmail.com>
2025-09-21 12:21:35 +0500

    test

--- database.py
+++ database.py
@@ -2,7 +2,7 @@
 
 from sqlalchemy import create_engine, Column, String, Integer, DateTime, Text, Boolean
 from sqlalchemy.orm import sessionmaker, Session
-
+import os
 
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./document_processor.db")
 engine = create_engine(DATABASE_URL)
