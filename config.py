7f531ec38a352454b887ba850031cd9b83621065
unknown <abbasi.work22@gmail.com>
2025-09-21 13:19:33 +0500

    test

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv  # <-- make sure python-dotenv is installed
 load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), ".env"))
+print("Current working dir:", os.getcwd())
 
 print("DOCSTRANGE_API_KEY1:", os.getenv("DOCSTRANGE_API_KEY1"))
 print("DOCSTRANGE_API_KEY2:", os.getenv("DOCSTRANGE_API_KEY2"))
